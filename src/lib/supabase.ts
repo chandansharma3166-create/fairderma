6675a878f68112a2689638a58e5854c75b52c963
Chandan Sharma <chandansharma3166@gmail.com>
2026-08-23 19:17:12 +0530

    Hardcode Supabase client config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://ucfoqrlsbzhmivkvoqnq.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjZm9xcmxzYnpobWl2a3ZvcW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0ODI5MDAsImV4cCI6MjEwMzA1ODkwMH0.13yd36YaONUWPEGfigdiDthtBUeXDc5rItUVz9Piy24";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
